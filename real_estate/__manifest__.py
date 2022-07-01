ea01d6819e3fed37cc30a61d5adb8a48cf191862
Sir Mwaniki Alvin IV <68748209+nipsalvin@users.noreply.github.com>
2022-07-01 11:01:11 +0300

    initial commit

---
+++ real_estate/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': "Alvin's Real Estate Module",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Nips Alvin",
+    'category': 'Category',
+    'installable': True,
+    'application': True,
+}
